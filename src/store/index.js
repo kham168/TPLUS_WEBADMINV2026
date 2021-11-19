ed4ca277fea801f988ac315bfb02439a7af7de60
bie <sidthiladsomphong@gmail.com>
2021-11-19 18:50:43 +0700

    add test

--- src/store/index.js
+++ src/store/index.js
@@ -4,42 +4,59 @@ import PostProvider from '@/resources/posts_provider'
 const postService = new PostProvider()
 
 Vue.use(Vuex)
+const state= {
+  roleDialog:false,
+  post:[],
+  res:[]
+}
+
+const mutations= {
+  IncrementRole(state){
+    state.roleDialog = !state.roleDialog;
+  },
+
+  SET_POST(state,data){
+    state.post = data;
+  },
+
+  SET_RES(state,data){
+    state.res = data;
+  }
+
+}
+
+const getters={
+  post:state => state.post,
+  res:state => state.res
+}
+
+const actions= {
+
+
+  async getPost ({commit}) {
+    const data = await postService.getPost()
+    console.log(data)
+    commit('SET_POST', data)
+  },
+  async createPost({commit},{title,body,userId}){
+
+    console.log(title)
+    console.log(body)
+    console.log(userId)
+    // console.log(datum[0].userId)
+    const data = await postService.createPost(title,body,userId)
+    commit('SET_RES',data)
+  }
+}
+
+const modules= {
+}
 
 export default new Vuex.Store({
-  state: {
-    roleDialog:false,
-    post:{},
-    res:{}
-  },
-  mutations: {
-    IncrementRole(state){
-      state.roleDialog = !state.roleDialog;
-    },
+  modules,
+  state,
+  getters,
+  actions,
+  mutations
 
-    SET_POST(state,data){
-      state.post = data;
-    },
-
-    SET_RES(state,data){
-      state.res = data;
-    }
- 
-  },
-  actions: {
-    async getPost ({commit}) {
-      const data = await postService.getPost()
-      commit('SET_POST', data)
-    },
-
-    async createPost({commit},datum){
-      console.log(datum)
-      console.log(datum[0].title)
-      console.log(datum[0].body)
-      console.log(datum[0].userId)
-      const data = await postService.createPost(datum[0].title,datum[0].body,datum[0].userId)
-      commit('SET_RES',data)
-    }
-  },
-  modules: {
-  }
 })
--- src/views/test/test.vue
+++ src/views/test/test.vue
@@ -1,41 +1,67 @@
 <template>
     <div id="TplusWebAdminTest">
         <h1>Test</h1>
+    
           <button @click="getPost()">get Post from API</button> <br>
-           <button @click="createPost(post)">create Post from API</button> <br>
-          {{post}}
-    {{res}}
+           <button @click="createPost({'title':'title','body':'body','userId':1})">create Post from API</button> <br>
+       
+
+
+  <v-data-table
+    :headers="headers"
+    :items="post"
+    :items-per-page="5"
+    class="elevation-1"
+
+  ></v-data-table>
+
+     
     </div>
 </template>
 
 <script>
 
-import {mapActions, mapState} from 'vuex'
+import {mapActions, mapGetters} from 'vuex'
 
 export default {
     name: 'TplusWebAdminTest',
 
     data() {
         return {
-            
+         
+             headers: [
+          {
+            text: 'id',
+            align: 'start',
+            sortable: false,
+            value: 'id',
+          },
+          { text: 'title', value: 'title' },
+          { text: 'body', value: 'body' },
+        
+        ],
+       
         };
     },
 
     mounted() {
-        
+       this.getPost();
     },
 
     methods: {
+
         ...mapActions({
-            getPost:'getPost',
-            createPost:'createPost'
-        })
+getPost:'getPost',
+createPost:'createPost'
+        } 
+        )
     },
 
     computed:{
-        ...mapState({
-        post:state=>state.post,
-        res:state=>state.res
+
+        ...mapGetters({
+        post:'post',
+        res:'res'
 
         })
     }
