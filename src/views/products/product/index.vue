4772ba3c0e1bb4e154d570a2be2f617735ae6aef
vongsavanh-dev <vongsavanhdev@gmail.com>
2021-12-28 11:15:40 +0700

    master-update

--- src/views/products/product/index.vue
+++ src/views/products/product/index.vue
@@ -15,24 +15,24 @@
       </div>
       <div class="product-content" v-show="!firstLoad">
         <v-data-table
-          :headers="$t('Product.table.headers')"
-          :items="product['data']"
-          :search="searchItem"
-          :loading="loading"
-          :loading-text="$t('Product.loadingtext')"
-          v-if="product['data'] != ''"
+            :headers="$t('Product.table.headers')"
+            :items="product['data']"
+            :search="searchItem"
+            :loading="loading"
+            :loading-text="$t('Product.loadingtext')"
+            v-if="product['data'] != ''"
         >
           <template v-slot:top>
             <v-toolbar flat>
               <v-text-field
-                :label="$t('Product.txtsearch')"
-                filled
-                rounded
-                dense
-                append-icon="fas fa-search"
-                single-line
-                hide-details
-                v-model="searchItem"
+                  :label="$t('Product.txtsearch')"
+                  filled
+                  rounded
+                  dense
+                  append-icon="fas fa-search"
+                  single-line
+                  hide-details
+                  v-model="searchItem"
               ></v-text-field>
               <v-spacer></v-spacer>
             </v-toolbar>
@@ -41,81 +41,40 @@
           <template v-slot:item="{ item, index }">
             <tr class="table-content" v-if="isLaoLanguage">
               <td>{{ index + 1 }}</td>
-<<<<<<< HEAD
-              <td>
-                <v-img
-                  :src="item.ProductImages[0].image"
-                  alt="preview"
-                  max-height="50"
-                  max-width="50"
-                ></v-img>
-              </td>
+              <td><v-img :src="item.ProductImages[0].image" alt="preview" max-height="50" max-width="50"></v-img></td>
               <td>{{ item.productName }}</td>
-              <td>
-                <p v-for="data in item.ProductCategories" :key="data.id">
-                  {{ data.cateProductId }}
-                </p>
-              </td>
-
-              <td
-                style="   max-width: 200px;
-=======
-                <td><v-img :src="item.ProductImages[0].image" alt="preview" max-height="50" max-width="50"></v-img></td>
-                 <td>{{ item.productName }}</td>
               <td ><p v-for="data in item.cateProducts" :key="data.id" >{{data.cateName}}</p></td>
-          
+
               <td style="   max-width: 200px;
->>>>>>> 87ed63aac7257d33ce97e2d7d83b52cbed225d4d
         overflow: hidden;
         text-overflow: ellipsis;
-        white-space: nowrap;"
-              >
-                {{ item.description }}
-              </td>
-              <td>
-                <v-btn icon @click="onShow(item.id)">
-                  <v-icon large>
-                    mdi-eye
-                  </v-icon></v-btn
-                >
-              </td>
+        white-space: nowrap;">{{ item.description }}</td>
+              <td><v-btn icon @click="onShow(item.id)"> <v-icon large>
+                mdi-eye
+              </v-icon></v-btn></td>
               <td>
                 <v-menu offset-y>
-                  <template v-slot:activator="{ on, attrs }">
+                  <template v-slot:activator="{on,attrs}">
                     <v-btn icon v-on="on" v-bind="attrs">
                       <v-icon small>fas fa-ellipsis-v</v-icon>
                     </v-btn>
                   </template>
                   <v-list>
-                    <v-list-item
-                      link
-                      @click="
-                        $router
-                          .push({
-                            name: 'product.edit',
-                            params: {
-                              product_id: item.id,
-                            },
-                          })
-                          .catch(() => {})
-                      "
-                    >
+                    <v-list-item link @click="$router.push({name:'product.edit',params:{
+                       'product_id':item.id,
+                   }}).catch(()=>{})">
                       <v-list-item-icon>
-                        <v-icon class="mr-3" small>{{
-                          $t("Product.table.options.iconEdit")
-                        }}</v-icon>
+                        <v-icon class="mr-3" small>{{$t('Product.table.options.iconEdit')}}</v-icon>
                         <v-list-item-title>
-                          {{ $t("Product.table.options.edit") }}
+                          {{$t('Product.table.options.edit')}}
                         </v-list-item-title>
                       </v-list-item-icon>
                     </v-list-item>
                     <v-list-item link @click="onDelete(item.id)">
                       <v-list-item-icon>
-                        <v-icon class="mr-3" small>{{
-                          $t("Product.table.options.delicon")
-                        }}</v-icon>
+                        <v-icon class="mr-3" small>{{$t('Product.table.options.delicon')}}</v-icon>
                         <v-list-item-title>
-                          {{ $t("Product.table.options.delete") }}
+                          {{$t('Product.table.options.delete')}}
                         </v-list-item-title>
                       </v-list-item-icon>
                     </v-list-item>
@@ -125,82 +84,41 @@
             </tr>
             <tr class="table-content" v-else>
               <td>{{ index + 1 }}</td>
-<<<<<<< HEAD
-              <td>
-                <v-img
-                  :src="item.ProductImages[0].image"
-                  alt="preview"
-                  max-height="50"
-                  max-width="50"
-                ></v-img>
-              </td>
+              <td><v-img :src="item.ProductImages[0].image" alt="preview" max-height="50" max-width="50"></v-img></td>
               <td>{{ item.ProductTrans[0].productName }}</td>
-              <td>
-                <p v-for="data in item.ProductCategories" :key="data.id">
-                  {{ data.cateProductId }}
-                </p>
-              </td>
-
-              <td
-                style="   max-width: 200px;
-=======
-                <td><v-img :src="item.ProductImages[0].image" alt="preview" max-height="50" max-width="50"></v-img></td>
-                <td>{{ item.ProductTrans[0].productName }}</td>
               <td ><p v-for="data in item.cateProducts" :key="data.id" >{{data.CateProductTrans[0].cateName}}</p></td>
-          
-           
+
+
               <td style="   max-width: 200px;
->>>>>>> 87ed63aac7257d33ce97e2d7d83b52cbed225d4d
         overflow: hidden;
         text-overflow: ellipsis;
-        white-space: nowrap;"
-              >
-                {{ item.description }}
-              </td>
-              <td>
-                <v-btn icon @click="onShow(item.id)">
-                  <v-icon large>
-                    mdi-eye
-                  </v-icon></v-btn
-                >
-              </td>
+        white-space: nowrap;">{{ item.description }}</td>
+              <td><v-btn icon @click="onShow(item.id)"> <v-icon large>
+                mdi-eye
+              </v-icon></v-btn></td>
               <td>
                 <v-menu offset-y>
-                  <template v-slot:activator="{ on, attrs }">
+                  <template v-slot:activator="{on,attrs}">
                     <v-btn icon v-on="on" v-bind="attrs">
                       <v-icon small>fas fa-ellipsis-v</v-icon>
                     </v-btn>
                   </template>
                   <v-list>
-                    <v-list-item
-                      link
-                      @click="
-                        $router
-                          .push({
-                            name: 'product.edit',
-                            params: {
-                              product_id: item.id,
-                            },
-                          })
-                          .catch(() => {})
-                      "
-                    >
+                    <v-list-item link @click="$router.push({name:'product.edit',params:{
+                       'product_id':item.id,
+                   }}).catch(()=>{})">
                       <v-list-item-icon>
-                        <v-icon class="mr-3" small>{{
-                          $t("Product.table.options.iconEdit")
-                        }}</v-icon>
+                        <v-icon class="mr-3" small>{{$t('Product.table.options.iconEdit')}}</v-icon>
                         <v-list-item-title>
-                          {{ $t("Product.table.options.edit") }}
+                          {{$t('Product.table.options.edit')}}
                         </v-list-item-title>
                       </v-list-item-icon>
                     </v-list-item>
                     <v-list-item link @click="onDelete(item.id)">
                       <v-list-item-icon>
-                        <v-icon class="mr-3" small>{{
-                          $t("Product.table.options.delicon")
-                        }}</v-icon>
+                        <v-icon class="mr-3" small>{{$t('Product.table.options.delicon')}}</v-icon>
                         <v-list-item-title>
-                          {{ $t("Product.table.options.delete") }}
+                          {{$t('Product.table.options.delete')}}
                         </v-list-item-title>
                       </v-list-item-icon>
                     </v-list-item>
@@ -218,29 +136,24 @@
         </div>
       </div>
       <ModalDelete>
-        <template v-slot="{ close }">
+        <template v-slot="{close}">
           <Delete :product_id="product_id" @close="close" />
         </template>
       </ModalDelete>
 
       <ModalShow>
-<<<<<<< HEAD
-        <template v-slot="{ close }">
-          <Show :product_id="product_id" @close="close" />
-=======
         <template v-slot="{close}">
           <Show
               :productTypeValue="productTypeValue"
               :productTypeValueEng="productTypeValueEng"
               :productName="productName"
-          :productNameEng="productNameEng"
-          :description="description"
-          :descriptionEng="descriptionEng"
-          :previewImage="previewImage"
-          :previewImageEng="previewImageEng"
+              :productNameEng="productNameEng"
+              :description="description"
+              :descriptionEng="descriptionEng"
+              :previewImage="previewImage"
+              :previewImageEng="previewImageEng"
 
               @close="close" />
->>>>>>> 87ed63aac7257d33ce97e2d7d83b52cbed225d4d
         </template>
       </ModalShow>
     </section>
@@ -248,26 +161,23 @@
 </template>
 
 <script>
-import { mapActions, mapGetters } from "vuex";
+import {mapActions,mapGetters} from 'vuex'
 import Delete from "@/components/forms/products/Product/Delete";
 import Show from "@/components/forms/products/Product/Show";
 import ModalShow from "@/components/Modals/modalShow";
 export default {
   name: "Product",
-
   components: {
     Delete,
     Show,
-    ModalShow,
+    ModalShow
   },
   data() {
     return {
-      product_id: "",
-      isLaoLanguage: localStorage.getItem("lang") === "la",
+      product_id:'',
+      isLaoLanguage:localStorage.getItem('lang') === 'la',
       loading: false,
       searchItem: "",
-
-
       productTypeValue:[],
       productTypeValueEng:[],
       productName:'',
@@ -279,100 +189,66 @@ export default {
       firstLoad:true,
     };
   },
-
   mounted() {
-<<<<<<< HEAD
-    this.getProduct();
-=======
-   
+
     this.getProduct().then((res)=>{
       if(res.success){
         this.firstLoad=false;
       }
     });
->>>>>>> 87ed63aac7257d33ce97e2d7d83b52cbed225d4d
   },
-
   methods: {
+
     CreateProduct() {
       this.$router
-        .push({
-          name: "product.create",
-        })
-        .catch(() => {});
+          .push({
+            name: "product.create",
+          })
+          .catch(() => {});
     },
     onDelete(product_id) {
-      this.product_id = product_id;
+      this.product_id = product_id
 
       this.$store.commit("modalDelete_State", true);
     },
     onShow(product_id) {
-<<<<<<< HEAD
-      this.product_id = product_id;
-=======
       this.productTypeValue=[];
       this.productTypeValueEng=[];
       this.previewImage=[];
       this.previewImageEng=[];
->>>>>>> 87ed63aac7257d33ce97e2d7d83b52cbed225d4d
-
       this.getProductOne({'product_id':product_id}).then(res=>{
         this.loadDataToComponent(res)
       })
       this.$store.commit("modalShow_State", true);
     },
-
     loadDataToComponent(res){
       let data = res.data;
-
-
       this.productName=data.productName
       this.productNameEng=data.ProductTrans[0].productName
       this.description=data.description
       this.descriptionEng=data.ProductTrans[0].description
-
       for(let i=0;i<data.cateProducts.length;i++){
-
         this.productTypeValue.push(data.cateProducts[i].cateName)
-
       }
       for(let i=0;i<data.cateProducts.length;i++){
-
         this.productTypeValueEng.push(data.cateProducts[i].CateProductTrans[0].cateName)
-
       }
-
       for(let i=0;i<data.ProductImages.length;i++){
-
         this.previewImage.push(data.ProductImages[i].image)
-
       }
-
-
-
-
-
     },
-
     ...mapActions({
-<<<<<<< HEAD
-      getProduct: "Product/getProduct",
-      getCateProductOne: "CateProduct/getCateProductOne",
-    }),
-=======
       getProduct:'Product/getProduct',
       getCateProductOne:'CateProduct/getCateProductOne',
       getProductOne:'Product/getProductOne'
     })
->>>>>>> 87ed63aac7257d33ce97e2d7d83b52cbed225d4d
   },
-
-  computed: {
+  computed:{
     ...mapGetters({
-      product: "Product/product",
-      cate_product_one: "CateProduct/cate_product_one",
-    }),
-  },
+      product:'Product/product',
+      cate_product_one:'CateProduct/cate_product_one'
+    })
+  }
 };
 </script>
 
@@ -380,11 +256,10 @@ export default {
 .product--section {
   width: 100%;
   background-color: $white-color;
-
   .product-content {
     width: 100%;
     padding: 1rem;
-    .text-limit {
+    .text-limit{
       max-width: 200px;
       overflow: hidden;
       text-overflow: ellipsis;
