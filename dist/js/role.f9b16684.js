904ec95e672eff8cfaeab8a9b85e81240f588059
sitthilath <sidthiladsomphong@gmail.com>
2022-03-12 13:33:00 +0700

    fix ip

--- dist/js/role.f9b16684.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["role"],{"4f2f":function(e,t,n){"use strict";n.r(t);var o=function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{attrs:{id:"Role"}},[n("router-view")],1)},r=[],u={name:"Role",data:function(){return{}},mounted:function(){},methods:{}},a=u,i=n("2877"),c=Object(i["a"])(a,o,r,!1,null,"5177eb82",null);t["default"]=c.exports}}]);
-//# sourceMappingURL=role.f9b16684.js.map
